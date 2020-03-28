25b372e3abe819fc5c2f658f31eb5d04d02f867a
Elvmx <elvmx47@aliyun.com>
2020-03-28 15:39:03 +0800

    控制器

---
+++ routers/postRouter.js
@@ -0,0 +1,29 @@
+// post路由文件
+
+// 引入 express
+const express = require("express");
+// 引入 postController
+const {
+  index,
+  create,
+  update,
+  remove
+} = require("../controllers/postController");
+
+// 生成 express.Router 的实例
+const router = express.Router();
+
+// GET /posts
+router.get("/", index);
+
+// POST /posts
+router.post("/", create);
+
+// PUT /posts/:id
+router.put("/:id", update);
+
+// DELETE /posts/:id
+router.delete("/:id", remove);
+
+// 暴露 router 的实例
+module.exports = router;
