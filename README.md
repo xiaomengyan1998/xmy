a435bc56aef45e21da0ffccd62716b7184ecca83
Elvmx <elvmx47@aliyun.com>
2020-03-28 17:27:23 +0800

    接口文档操作

--- README.md
+++ README.md
@@ -15,3 +15,33 @@
 1. M model 数据层
 2. V view 视图层
 3. C controller 控制层
+
+## 四、接口文档
+
+#### 使用步骤
+
+1. 安装 apidoc
+
+```bash
+$ npm install apidoc
+```
+
+apidoc 提供了一个 apidoc 的命令，可以通过 apidoc -v 去验证。
+
+由于这块我们是本地安装，要验证 apidoc 命令的话有如下三种方式：
+
+    - cd node_modules/.bin 再去 apidoc -v
+    - npx apidoc -v   (npx 是 npm 5.x 版本之后提供的一个命令)
+    - package.json 中写脚本
+
+2. 在每一个路由代码前写上 apidoc 规定的注释信息
+3. 在项目根目录下创建一个 apidoc.json 文件，配置 api 接口文档的一些描述信息
+4. 通过 apidoc 的命令去生成 api 接口文档
+
+```bash
+$ apidoc -i 写注释的路径 -o 文档输出路径
+
+$ apidoc -i ./routers -o ./public/docs
+```
+
+5. 访问文档
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -13,16 +13,50 @@ const {
 // 生成 express.Router 的实例
 const router = express.Router();
 
-// GET /posts
+/**
+ * @api {get} /posts 获取帖子列表
+ * @apiName index
+ * @apiGroup Post
+ *
+ * @apiSuccess {Number} code 错误状态码.
+ * @apiSuccess {String} msg  错误消息.
+ * @apiSuccess {Array} data 帖子数组.
+ */
 router.get("/", index);
 
-// POST /posts
+/**
+ * @api {post} http://localhost:3000/posts 创建一个帖子
+ * @apiName create
+ * @apiGroup Post
+ *
+ * @apiParam {String} title 帖子标题
+ * @apiParam {String} content 帖子内容
+ *
+ * @apiSuccess {Number} code 错误状态码.
+ * @apiSuccess {String} msg  错误消息.
+ */
 router.post("/", create);
 
-// PUT /posts/:id
+/**
+ * @api {put} http://localhost:3000/posts/:id 编辑帖子
+ * @apiName update
+ * @apiGroup Post
+ *
+ * @apiParam {String} title 帖子标题
+ * @apiParam {String} content 帖子内容
+ *
+ * @apiSuccess {Number} code 错误状态码.
+ * @apiSuccess {String} msg  错误消息.
+ */
 router.put("/:id", update);
 
-// DELETE /posts/:id
+/**
+ * @api {delete} http://localhost:3000/posts/:id 删除帖子
+ * @apiGroup Post
+ *
+ * @apiSuccess {Number} code 错误状态码.
+ * @apiSuccess {String} msg  错误消息.
+ */
 router.delete("/:id", remove);
 
 // 暴露 router 的实例
